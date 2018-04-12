fa1cc7850a329512839e80b9568b0b48480f950e
PederHA <peder.andresen@gmail.com>
2018-04-12 21:17:48 +0200

    Comments

--- Spotify/NowPlaying_Win32API/nowplaying_win32gui.py
+++ Spotify/NowPlaying_Win32API/nowplaying_win32gui.py
@@ -5,23 +5,35 @@ import win32gui
 This script used to be based on Spotilib, however Spotify broke it in
 some Q2 2018 update. I have rewritten the script to restore its functionality.
 However, memory footprint has quadrupled as a result. From 1MB to 4MB.
+
+Works as of Spotify 1.0.77.338.g758ebd78
 """
 
 def find_active_window():
+    """
+    Tries to find an active Spotify window. Loops until window is found.
+    """
 
+    # Looks for a window with className "Chrome_WidgetWin_0" and title "Spotify"
     window_id = win32gui.FindWindow("Chrome_WidgetWin_0", "Spotify")
-
+    
+    # If win32gui.FindWindow returned 0, repeat until window is found.
     while window_id == 0:
         window_id = win32gui.FindWindow("Chrome_WidgetWin_0", "Spotify")
         time.sleep(2)
+   # Run get_song_info with the window_id that was obtained
     get_song_info(window_id)
 
 def get_song_info(window_id):
-    
-    spotify_window_active = True
+    """
+    Gets Spotify window title using `win32.GetWindowText` and the
+    previously obtained `window_id`
+    """
 
+    spotify_window_active = True
+    
     while spotify_window_active == True:
-        #Get spotify window info.
+        # Get spotify window info.
         spotify_text = win32gui.GetWindowText(window_id)
         artist = ""
         song = ""
@@ -35,7 +47,7 @@ def get_song_info(window_id):
         else:
             artist, song = spotify_text.split("-",1)
 
-            #Concatenate
+            # Concatenate
             songinfo = (artist + " - " + song + "       ")
 
             with open('np.txt','w',encoding="utf-8") as f:
