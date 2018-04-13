7587986c2a37f2cfa5347407638978f704be0e79
PederHA <peder.andresen@gmail.com>
2018-04-13 21:19:18 +0200

    Cleaned up a little bit. Still inferior to win32gui-based script.

--- Spotify/NowPlayingAPI/np.py
+++ Spotify/NowPlayingAPI/np.py
@@ -2,8 +2,12 @@ import sys
 import spotipy
 import spotipy.util as util
 import json
-from pprint import pprint
 import time
+
+"""
+Based on Example from spotipy. 
+Not very good.
+"""
 
 scope = 'user-read-playback-state'
 
@@ -18,23 +22,19 @@ while True:
     token = util.prompt_for_user_token(username, scope)
     if token:
         sp = spotipy.Spotify(auth=token)
+        
         results = sp.current_user_playing_track()
         item = results['item']
-        #for artists in item["album"]["artists"]:
-            #print (artists)
-        print ("Artist:",item["artists"][0]["name"])
-        print ("Song title:",item["name"])
-        artistw = json.dumps(item["artists"][0]["name"])
-        artistw = str(artistw)
-        artistw = artistw.replace('"', '')
-        songw = json.dumps(item["name"])
-        songw = songw.strip('\"')
-        f = open('np.txt','w')
-        f.write(artistw)
-        f.write(' - ')
-        f.write(songw)
-        f.write('      ')
-        f.close
+
+        artist = json.dumps(item["artists"][0]["name"])
+        artist = str(artist).replace('"', '')
+        
+        song = json.dumps(item["name"])
+        song = song.strip('\"')
+
+        with open('np.txt','w') as f:
+            f.write(artist + ' - ' + song + '      ')
+
 
     else:
         print ("Can't get token for", username)
