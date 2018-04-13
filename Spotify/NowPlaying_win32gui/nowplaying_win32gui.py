fd0558dc59d8ab3e71006c5be49e957fdb4b0e9e
PederHA <peder.andresen@gmail.com>
2018-04-13 03:52:20 +0200

    Fully commented

--- Spotify/NowPlaying_win32gui/nowplaying_win32gui.py
+++ Spotify/NowPlaying_win32gui/nowplaying_win32gui.py
@@ -2,9 +2,10 @@ import time
 import win32gui
 
 """
-This script used to be based on Spotilib, however Spotify broke it in
-some Q2 2018 update. I have rewritten the script to restore its functionality.
-However, memory footprint has quadrupled as a result. From 1MB to 4MB.
+This script used to be based on Spotilib, however its functionality
+was broken in a Q2 2018 Spotify update. I have rewritten the script 
+to restore its functionality.However, memory footprint has quadrupled 
+as a result. From 1MB to 4MB.
 
 Works as of Spotify 1.0.77.338.g758ebd78
 """
@@ -16,6 +17,7 @@ def find_active_window():
     """
 
     # Looks for a window with className "Chrome_WidgetWin_0" and title "Spotify"
+    # Returns 0 if window isn't found.
     window_id = win32gui.FindWindow("Chrome_WidgetWin_0", "Spotify")
 
     # If win32gui.FindWindow returned 0, repeat until window is found.
@@ -30,24 +32,35 @@ def get_song_info(window_id):
     """
     Gets Spotify window title using `win32gui.GetWindowText()` with the
     previously obtained `window_id`. If title is not Spotify, parse
-    title and write it to text document.
+    title and write it to text document. 
+    
+    After song info is parsed and
+    written, sleeps for 10 seconds then resumes the loop as long as
+    a Spotify window is active.
+
+    Should the window be closed, the loop is exited and `find_active_window()`
+    is executed again.
     """
 
     spotify_window_active = True
 
     while spotify_window_active:
-        # Get spotify window info.
+        # Get Spotify window title.
         spotify_text = win32gui.GetWindowText(window_id)
         artist = ""
         song = ""
+        
         if spotify_text == "Spotify":
+            # This means a song isn't currently playing, but the application is running.
             time.sleep(5)
 
         elif spotify_text == '':
+            # If the spotify window that was found with find_active_window() is not found, terminate loop.
             spotify_window_active = False
             find_active_window()
 
         else:
+            # Else split the window title at the "-" symbol to get artist and song strings.
             artist, song = spotify_text.split("-", 1)
 
             # Concatenate
