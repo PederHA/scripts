1f142014aa11bcc242096826bfa5b8a5f7d278b2
PederHA <peder.andresen@gmail.com>
2018-04-15 19:02:48 +0200

    Fixed a small error in doscstring.

--- Spotify/NowPlaying_win32gui/nowplaying_win32gui.py
+++ Spotify/NowPlaying_win32gui/nowplaying_win32gui.py
@@ -10,8 +10,8 @@ displaying "Now Playing" info when streaming.
 This script used to be based on Spotilib, however its functionality
 was broken in a Q2 2018 Spotify update. I have rewritten the script 
 to restore its functionality. However, memory footprint has increased 
-as a result. From 1MB to 4MB. Sleep values can be tweaked, but based
-on personal testing, polling every 2/5 seconds and writing every 10 
+as a result. From 1MB to 4MB. Sleep values can be tweaked according 
+to personal preferences.
 
 Works as of Spotify 1.0.77.338.g758ebd78
 """
